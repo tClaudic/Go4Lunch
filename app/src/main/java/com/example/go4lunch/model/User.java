bd25ea2ac897b6bd00581ed47bf9fecb25cfc535
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-05-23 12:53:11 +0200

    User Model modified

--- app/src/main/java/com/example/go4lunch/model/User.java
+++ app/src/main/java/com/example/go4lunch/model/User.java
@@ -1,6 +1,26 @@
 package com.example.go4lunch.model;
 
+import com.google.firebase.firestore.Exclude;
+
 import java.io.Serializable;
 
 public class User implements Serializable {
+
+    public String uid;
+    public String name;
+    @SuppressWarnings("WeakerAccess")
+    public String email;
+    @Exclude
+    public boolean isAuthenticated;
+    @Exclude
+    boolean isNew, isCreated;
+
+    public User() {
+    }
+
+    User(String uid, String name, String email) {
+        this.uid = uid;
+        this.name = name;
+        this.email = email;
+    }
 }
