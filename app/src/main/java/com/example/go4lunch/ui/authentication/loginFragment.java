0e15d75d6a277300cc82399257a68e437d412398
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-06-01 19:01:49 +0200

    Add ActivityResultLauncher

--- app/src/main/java/com/example/go4lunch/ui/authentication/loginFragment.java
+++ app/src/main/java/com/example/go4lunch/ui/authentication/loginFragment.java
@@ -1,12 +1,16 @@
 package com.example.go4lunch.ui.authentication;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.IntentSenderRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -21,23 +25,33 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
 public class loginFragment extends Fragment {
 
+    private static final int RC_SIGN_IN = 123;
     private GoogleSignInClient googleSignInClient;
     private FragmentLoginBinding binding;
 
-    public loginFragment() {
-        super(R.layout.fragment_login);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
     }
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentLoginBinding.inflate(getLayoutInflater());
+        initSignInButton();
+        initGoogleSignInClient();
         return binding.getRoot();
     }
+
+
 
     private void initGoogleSignInClient() {
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions
@@ -57,17 +71,34 @@ public class loginFragment extends Fragment {
 
     private void signIn() {
         Intent signInIntent = googleSignInClient.getSignInIntent();
+        googleSignInActivityResultLauncher.launch(signInIntent);
+
+
+
 
     }
 
-    private ActivityResultLauncher<IntentSenderRequest> loginResultHandler =
-            registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(),
-                    result -> {
+  ActivityResultLauncher<Intent> googleSignInActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK){
+                        Log.e("Login","Succesfluyy logged");
                         Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
-                        handleSignInResult(task);
+                        GoogleSignInAccount googleSignInAccount = task.getResult();
+                        if (googleSignInAccount != null){
+                            getGoogleAuthCredential(googleSignInAccount);
+                        }
+                    }
 
+                }
+            }
+    );
 
-                    });
+    private void getGoogleAuthCredential(GoogleSignInAccount googleSignInAccount) {
+
+    }
 
     private void handleSignInResult(Task<GoogleSignInAccount> task) {
     }
