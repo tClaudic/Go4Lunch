5a1f0b6945aaef2f416b2715f9d185a5f10b4986
Tristan <55841804+Haagentiv1@users.noreply.github.com>
2022-06-06 14:28:12 +0200

    google maps sdk added and configured + location provider solution started

---
+++ app/src/main/java/com/example/go4lunch/model/LocationModel.java
@@ -0,0 +1,12 @@
+package com.example.go4lunch.model;
+
+public class LocationModel {
+
+    public LocationModel(Double longitude, Double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Double longitude;
+    public Double latitude;
+}
--- app/src/main/java/com/example/go4lunch/ui/mapView/MapFragment.java
+++ app/src/main/java/com/example/go4lunch/ui/mapView/MapFragment.java
@@ -1,12 +1,25 @@
 package com.example.go4lunch.ui.mapView;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.go4lunch.R;
+import com.example.go4lunch.databinding.FragmentMapBinding;
 
 public class MapFragment extends Fragment {
 
-    public MapFragment(){
-        super(R.layout.fragment_map);
+    FragmentMapBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentMapBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
     }
 }
