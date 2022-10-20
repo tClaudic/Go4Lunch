e4f465880ca751ddae4ead45b0839e1bb0870bbb
Haagentiv1 <55841804+Haagentiv1@users.noreply.github.com>
2022-10-20 11:39:19 +0200

    refactoring, improving methods, add some String Ressouces in EmailSignInFragment

--- app/src/main/java/com/example/go4lunch/ui/authentication/EmailSignInFragment.java
+++ app/src/main/java/com/example/go4lunch/ui/authentication/EmailSignInFragment.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,12 +17,13 @@ import com.example.go4lunch.R;
 import com.example.go4lunch.databinding.FragmentSignInBinding;
 import com.example.go4lunch.ui.ViewModelFactory;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailSignInFragment extends Fragment {
 
-    FragmentSignInBinding binding;
+    private FragmentSignInBinding binding;
     private AuthViewModel authViewModel;
     private NavController navController;
     public static final String SUCCESS = "success";
@@ -50,43 +50,28 @@ public class EmailSignInFragment extends Fragment {
         authViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(AuthViewModel.class);
     }
 
-    public void goToMainFragment(){
+    public void goToMainFragment() {
         Navigation.findNavController(binding.getRoot()).navigate(R.id.nav_mapView);
     }
 
 
-
-
     private void setupNavToSignUp() {
-        binding.tvGoToSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                navController.navigate(R.id.action_nav_SignInFragment_to_emailSignUpFragment);
-            }
-        });
+        binding.tvGoToSignIn.setOnClickListener(view -> navController.navigate(R.id.action_nav_SignInFragment_to_emailSignUpFragment));
     }
 
     private void setupLoginButton() {
-        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                resetErrorMessage();
-                if (emailFormatVerification() && passwordVerification()) {
-                    Toast.makeText(getContext(), "all is okay", Toast.LENGTH_LONG).show();
-                    authViewModel.signInWithMailAndPassword(binding.etRegisterEmail.getText().toString(), binding.etRegisterPassword.getText().toString()).observe(getViewLifecycleOwner(), new Observer<String>() {
-                        @Override
-                        public void onChanged(String s) {
-                            if (s.equals(SUCCESS)){
+        binding.btnSignUp.setOnClickListener(view -> {
+            resetErrorMessage();
+            if (emailFormatVerification() && passwordVerification()) {
+                Toast.makeText(getContext(), "all is okay", Toast.LENGTH_LONG).show();
+                authViewModel.signInWithMailAndPassword(Objects.requireNonNull(binding.etRegisterEmail.getText()).toString(), Objects.requireNonNull(binding.etRegisterPassword.getText()).toString())
+                        .observe(getViewLifecycleOwner(), result -> {
+                            if (result.equals(SUCCESS)) {
                                 goToMainFragment();
-                            }else if (s.equals(ERROR)){
-                                Toast.makeText(requireActivity(),"erro",Toast.LENGTH_LONG).show();
+                            } else if (result.equals(ERROR)) {
+                                Toast.makeText(requireActivity(), R.string.error_during_authentication, Toast.LENGTH_LONG).show();
                             }
-                        }
-                    });
-
-                }
-
-
+                        });
             }
         });
 
@@ -99,8 +84,8 @@ public class EmailSignInFragment extends Fragment {
 
     public Boolean passwordVerification() {
         boolean passwordVerification = false;
-        if (binding.etRegisterPassword.getText().toString().isEmpty()) {
-            binding.tilRegisterPassword.setError("you cannot let this field empty");
+        if (Objects.requireNonNull(binding.etRegisterPassword.getText()).toString().isEmpty()) {
+            binding.tilRegisterPassword.setError(getString(R.string.empty_field));
         } else {
             passwordVerification = true;
         }
@@ -109,16 +94,16 @@ public class EmailSignInFragment extends Fragment {
 
 
     public Boolean emailFormatVerification() {
-        String regex = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
+        String regex = "^([a-zA-Z0-9_\\-.]+)@([a-zA-Z0-9_\\-.]+)\\.([a-zA-Z]{2,5})$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(binding.etRegisterEmail.getText().toString());
+        Matcher matcher = pattern.matcher(Objects.requireNonNull(binding.etRegisterEmail.getText()).toString());
         if (binding.etRegisterEmail.getText().toString().isEmpty()) {
-            binding.tilRegisterEmail.setError("you cannot let this field empty");
+            binding.tilRegisterEmail.setError(getString(R.string.empty_field));
             return false;
 
         }
         if (!matcher.matches()) {
-            binding.tilRegisterEmail.setError("The enmail is the wronf format please follow as aaaa@aaaa.com");
+            binding.tilRegisterEmail.setError(getString(R.string.wrong_email_format));
             return false;
         }
         return true;
