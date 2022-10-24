132ee28d016e5f6cb19d6c2fa3eb1751724aa1ed
Haagentiv1 <55841804+Haagentiv1@users.noreply.github.com>
2022-10-24 22:06:59 +0200

    remove unnecessary method from SplashFragment

--- app/src/main/java/com/example/go4lunch/ui/splashScreen/SplashFragment.java
+++ app/src/main/java/com/example/go4lunch/ui/splashScreen/SplashFragment.java
@@ -8,13 +8,11 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import com.example.go4lunch.R;
 import com.example.go4lunch.databinding.FragmentSplashScreenBinding;
-import com.example.go4lunch.model.User;
 import com.example.go4lunch.ui.ViewModelFactory;
 import com.example.go4lunch.ui.authentication.AuthViewModel;
 
@@ -22,10 +20,10 @@ import java.util.Objects;
 
 public class SplashFragment extends Fragment {
 
-    private FragmentSplashScreenBinding binding;
-    AuthViewModel authViewModel;
     public static final String SUCCESS = "success";
     public static final String ERROR = "error";
+    AuthViewModel authViewModel;
+    private FragmentSplashScreenBinding binding;
 
     @Nullable
     @Override
@@ -36,22 +34,18 @@ public class SplashFragment extends Fragment {
         return binding.getRoot();
     }
 
-    private void initAuthViewModel(){
+    private void initAuthViewModel() {
         authViewModel = new ViewModelProvider(this, ViewModelFactory.getInstance()).get(AuthViewModel.class);
     }
 
-    private void checkIfUserIsAuthenticated(){
+    private void checkIfUserIsAuthenticated() {
         authViewModel.checkIfUserIsAuthenticated().observe(getViewLifecycleOwner(), result -> {
-            if (!Objects.equals(result, ERROR)){
-                getUserFromDatabase(result);
+            if (!Objects.equals(result, ERROR)) {
                 Navigation.findNavController(binding.getRoot()).navigate(R.id.nav_mapView);
-            }else {
+            } else {
                 Navigation.findNavController(binding.getRoot()).navigate(R.id.nav_login);
             }
         });
-    }
-    private void getUserFromDatabase(String uid){
-        authViewModel.getUserById(uid);
     }
 
 }
